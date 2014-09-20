6b10ca1f61709d0f7c9b902f8211a3585424c1ce
Marek Caputa <m.caputa@selleo.com>
2014-09-20 15:08:06 +0200

    Add event controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,37 @@
+class EventsController < ApplicationController
+  expose(:event, attributes: :event_params)
+  expose(:events)
+
+
+  def index
+
+  end
+
+  def create
+    creator_id = current_user
+    if event.save
+      redirect_to event, notice: t('events.create.success')
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if event.save
+      redirect_to event, notice: t('events.update.success')
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    event.destroy
+    redirect_to events_url, notice: t('events.destroy.success')
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :start_at, :end_at)
+  end
+end
