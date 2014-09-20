a1fa44aa0e5123bdbb1dfb35f7ea6846686aa57e
stevo <blazejek@gmail.com>
2014-09-20 11:27:52 +0200

    Handle facebook authorization callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :omniauthable, omniauth_providers: [:facebook]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.nickname = auth.info.name
+    end
+  end
 end
