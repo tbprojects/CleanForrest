b1c133411dd274af76292d8ea13d0336e7faddbb
tbprojects <info.tbprojects@gmail.com>
2014-09-20 11:55:31 +0200

    cms setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  comfy_route :cms_admin, :path => '/admin'
   devise_for :users
   root to: 'visitors#index'
+  # Make sure this routeset is defined last
+  comfy_route :cms, path: '/', sitemap: false
 end
