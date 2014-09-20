8942591812a594ad9a40c884974f3c8045a345d2
Marek Caputa <m.caputa@selleo.com>
2014-09-20 14:37:35 +0200

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
+
   devise_scope :user do
     get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
 
+  resources :events
+
+  resource :my_profile, only: [:show, :edit, :update], controller: 'my_profile'
+
+  comfy_route :cms_admin, :path => '/admin'
+  root to: 'visitors#index'
   comfy_route :cms_admin, path: '/admin'
   comfy_route :blog_admin, path: 'admin'
 
@@ -11,6 +18,5 @@ Rails.application.routes.draw do
   root to: 'comfy/blog/posts#index'
 
   # Make sure this routeset is defined last
-  comfy_route :blog, path: 'blog'
   comfy_route :cms, path: '/', sitemap: false
 end
