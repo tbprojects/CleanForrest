a9321267b91be50fff537796cd1c006a0e3b5639
Marek Caputa <m.caputa@selleo.com>
2014-09-20 14:59:46 +0200

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,14 @@ Rails.application.routes.draw do
     get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
 
-  resources :events
-
-  resource :my_profile, only: [:show, :edit, :update], controller: 'my_profile'
-
-  comfy_route :cms_admin, :path => '/admin'
-  root to: 'visitors#index'
   comfy_route :cms_admin, path: '/admin'
   comfy_route :blog_admin, path: 'admin'
 
-  resource :my_profile, only: [:show, :edit, :update], controller: 'my_profile'  
+  resource :my_profile, only: [:show, :edit, :update], controller: 'my_profile'
+  resources :events
   root to: 'comfy/blog/posts#index'
 
   # Make sure this routeset is defined last
   comfy_route :cms, path: '/', sitemap: false
+  comfy_route :blog, path: 'blog'
 end
